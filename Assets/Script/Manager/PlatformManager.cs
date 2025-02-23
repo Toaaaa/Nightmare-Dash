8fe91478f8e7f217bc4a3d47c5c34cb44ac9ef2c
kim minjun <k980128@naver.com>
2025-02-23 19:33:56 +0900

    refactor : 맵 생성 패턴 수정 (자동 생성 >> 사전 배치)

--- Assets/Script/Manager/PlatformManager.cs
+++ Assets/Script/Manager/PlatformManager.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class PlatformManager : MonoBehaviour
 {
-    [SerializeField] float speed = 1.5f; // 이동 속도 + 다음 플렛폼 생성시간 (임시로 플랫폼 매니저에서 관리)
-    [SerializeField] Platform startPlatform; // 시작 플랫폼
-    [SerializeField] Platform platformPrefab; // 플랫폼 프리팹
-    [SerializeField] List<Obstacle> obstacles; // 장애물 리스트
+    // 변수
+    [SerializeField] float speed = 1.5f; // 이동 속도
+    //[SerializeField] SpawnPoint spawnPoint; // 플랫폼 생성 위치
+
+    // 이니셜 데이터 + 프리팹
+    [SerializeField] GameObject startPlatform; // 시작 플랫폼
+    [SerializeField] Platform platformPrefab; // 플랫폼 프리팹 (발판 1칸길이의 프리팹)
+
+
+    // 데이터 베이스
     [SerializeField] List<RunningItem> runningItems; // 피격시 사용되는 아이템 리스트
 
-    // 1. 플렛폼은 랜덤 생성 (ground level의 기본 플렛폼과 공중 플렛폼 존재)
-    // 2. 장애물은 플렛폼의 중간 일정 비율 범위 내에서만 생성되며, 플렛폼의 길이에 따라 갯수가 결정된다.
 
     void Start()
     {
@@ -36,17 +40,14 @@ public class PlatformManager : MonoBehaviour
     }
 
 
-    void InitalPlatform() // 초기 플랫폼 생성 (시작 플렛폼 뒤로 3개)
+    public float GetSpeed()// 플렛폼 속도.
+    {
+        return speed;
+    }
+
+    void InitalPlatform() // 초기 플랫폼 생성 (시작 플렛폼)
     {
         
-    }
-    void CreatePlatform()
-    {
-        // 플랫폼 생성
-    }
-    void CreateObstacle()
-    {
-        // 장애물 생성
     }
     void CreateRunningItem()
     {
