c5f359af5c33f1a77196cd81452d7885f21b7d86
kierwl <45115106+kierwl@users.noreply.github.com>
2025-02-24 11:49:22 +0900

    feat: 설정 버튼 기능 구현
    Exit 종료, Back 되돌아가기(UI닫기) 구현

--- Assets/Script/UI/SettingUI.cs
+++ Assets/Script/UI/SettingUI.cs
@@ -45,6 +45,10 @@ public class SettingUI : MonoBehaviour
 
     private void QuitGame()
     {
-        Application.Quit();  // 게임 종료
+    #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;  // 에디터에서 실행 중지
+    #else
+        Application.Quit();  // 빌드된 게임 종료
+    #endif
     }
 }
