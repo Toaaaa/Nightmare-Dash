320cf5a4bf2038cfb855bb32ad6dd2ddb0883b95
LeeJiyoung <cjstk7597@gmail.com>
2025-02-24 10:25:04 +0900

    feat 체력바와 점수 UI 구현

--- Assets/Script/SceneController/GameSceneController.cs
+++ Assets/Script/SceneController/GameSceneController.cs
@@ -1,11 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameSceneController : SceneBase
 {
+    //점수
+    public int Score
+    {
+        get => score;
+        set
+        {
+            score = value;
+            OnChangedScoreAmountEvnet(score);
+        }
+    }
+
+    private int score = 0;
+
+    //점수값이 변할때 발생하는 이벤트
+    public Action<int> OnChangedScoreAmountEvnet = delegate { };
+
+    [SerializeField]
+    private InGameUIController uiController;
+
     protected override void OnStart(object data)
     {
         base.OnStart(data);
+        uiController.Initialize();
     }
 }
