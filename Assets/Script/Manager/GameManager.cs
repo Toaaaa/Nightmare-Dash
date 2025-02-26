8a8738455fbb82f520d8715c6d5634df9d1415f4
Kimahyeon <kay877@naver.com>
2025-02-26 21:18:40 +0900

    플레이어 유물 획득

--- Assets/Script/Manager/GameManager.cs
+++ Assets/Script/Manager/GameManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    public PlayerData playerData; // ✅ PlayerData를 참조
 
     private void Awake()
     {
