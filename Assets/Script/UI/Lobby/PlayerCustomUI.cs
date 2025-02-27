bb2cab45187389cb35b3818f2df78393db3c930e
kierwl <45115106+kierwl@users.noreply.github.com>
2025-02-27 18:44:39 +0900

    feat:불필요요소 제거

--- Assets/Script/UI/Lobby/PlayerCustomUI.cs
+++ Assets/Script/UI/Lobby/PlayerCustomUI.cs
@@ -34,6 +34,7 @@ public class PlayerCustomUI : BaseUI
 
     [Header("Description")]
     [SerializeField] private TMP_Text descriptionText;
+    [SerializeField] private Image previewimg;
 
     public static PlayerCustomUI Instance { get; private set; }
     private void Awake()
@@ -41,6 +42,8 @@ public class PlayerCustomUI : BaseUI
         if (Instance == null) Instance = this;
         else Destroy(gameObject);
         GameManager.instance.LoadPlayerData();
+
+
     }
     private void Start()
     {
@@ -68,6 +71,7 @@ public class PlayerCustomUI : BaseUI
 
         LoadArtifactSlots();
     }
+   
 
 
     public void UpdateAchievementUI(string achievementName = null)
@@ -168,12 +172,14 @@ public class PlayerCustomUI : BaseUI
 
         // 기존 슬롯 삭제
         foreach (Transform child in relicSlotContainer) Destroy(child.gameObject);
+       
         // 플레이어가 보유한 유물 리스트 가져오기
         List<ArtifactData> ownedArtifacts = GameManager.instance.playerData.OwnedArtifacts;
         Debug.Log($"OwnedArtifacts 개수: {ownedArtifacts.Count}");
         // 유물 슬롯 생성
         foreach (var artifact in ownedArtifacts)
         {
+            Debug.Log("슬롯 생성");
             GameObject newSlot = Instantiate(relicslotPrefab, relicSlotContainer);
             TMP_Text slotText = newSlot.GetComponentInChildren<TMP_Text>();
             if (slotText != null)
@@ -184,7 +190,7 @@ public class PlayerCustomUI : BaseUI
             if (slotImage != null)
             {
                 slotImage.sprite = artifact.ArtifactImage;
-
+                
                 slotbackgroundimg.color = Color.white;
                 slotbackgroundimg.enabled = true;
                 slotImage.enabled = true;
@@ -204,6 +210,15 @@ public class PlayerCustomUI : BaseUI
         descriptionText.text = $"{itemName} 설명을 여기에 표시";
         descriptionText.DOFade(0, 0);  // 투명하게 초기화
         descriptionText.DOFade(1, 0.3f); // 점점 나타나는 효과
+
+       /* if (previewimg != null)
+        {
+            previewimg.sprite = spriteImg;
+            previewimg.color = new Color(1, 1, 1, 1); // 투명도를 초기화 (이미지 표시)
+        }
+        previewimg.DOFade(0, 0);
+        previewimg.DOFade(1, 0.3f);*/
+
     }
 
     private void CloseUI()
